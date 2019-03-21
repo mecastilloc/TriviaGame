cae61df76561177b7862e593c3bf5acf33e4e5bf
ManuCas <manucastle@hotmail.com>
2019-03-21 13:39:41 -0600

    added restart game without reload, fixed bug with timers: cuase... the interval. final css styling.

--- README.md
+++ README.md
@@ -1,19 +1,41 @@
 # TriviaGame
-week 5 homework
+week 5 homework Trivia Game
+
+
+### Option Two: Advanced Assignment (Timed Questions)
+
+**[Live Demo](https://mecastilloc.github.io/TriviaGame/index.html)**.
+
+* This trivia game is about Formula One knowledge. When playng it shows only one question until the player answers it or their time of 15 seconds runs out.
+
+* If the player selects the correct answer, it shows a screen congratulating them for choosing the right option. After a few seconds, the next question will be displayed (without user input).
+
+* The scenario for wrong answers and time-outs is:
+
+  * If the player runs out of time, it tells the player that time's up and displays the correct answer. Waits a few seconds, then shows the next question.
+  * If the player chooses the wrong answer, it tells the player they selected the wrong option and then displays the correct answer. Waits a few seconds, then shows the next question.
+
+* On the final screen, shows the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
 
 
 
 commit 1:
-Initial file structure
+Initial commit, repo created
 
 commit 2:
+Initial file structure
+
+commit 3:
 basic  function, change question, shw not answered warning
 
-commit3:
+commit 4:
 bug not showing last correct or incorrect message is fixed. New timers logic. Add new question to seek for errors
 
-commit 4:
+commit 5:
 click events added whit a generic function, final stats oparates correctly.
 
-commit 5:
-all functions added, missing real questions and last css details
+commit 6:
+all functions added, missing real questions and last css details.
+
+commit 7:
+added restart game without reload, fixed bug with timers: cuase... the interval. final css styling.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,70 @@
 var q1 = {
-    q: "cual es mi nombre",
-    a: ["manuel", "ricardo", "chicho", "lola"],
-    v: ["correct", "incorrect", "incorrect", "incorrect"],
+    q: "Who has won more F1 titles?",
+    a: ["a)Lewis Hamilton", "b) Michael Schumacher", "c) Ayrton Senna", "d) Sebastian Vettel"],
+    v: ["incorrect", "correct", "incorrect", "incorrect"],
 
 
 }
 
 var q2 = {
-    q: "cual es mi apellido",
-    a: ["casas", "chozas", "castillo", "hoetels"],
-    v: ["incorrect", "correct", "incorrect", "incorrect"],
-    
+    q: "In 2016, who became F1 World Champion and then announced his retirement from the sport five days ",
+    a: ["a) Jenson Button", "b) Felipe Massa", "c) Nico Rosberg", "d) Fernando Alonso"],
+    v: ["incorrect", "incorrect", "correct", "incorrect"],
+
 
 }
 
 var q3 = {
-    q: "cual es mi segundo",
-    a: ["casas", "chozas", "castillo", "casas2"],
-    v: ["incorrect", "incorrect", "correct", "incorrect"],
-    
-
+    q: "How many points an F1 driver can win on a Grand Prix Weekend?",
+    a: ["a) 25 points", "b) 26 points", "c) 8 points", "d) 17 points"],
+    v: ["incorrect", "correct", "incorrect", "incorrect"],
 }
 
 
 var q4 = {
-    q: "cual perro tengo",
-    a: ["casas", "chozas", "chihuahua", "st bernardo"],
+    q: "Who was the first F1 Grand Prix Champion in 1950?",
+    a: ["a) Juan Fangio", "b) Peter Collins", "c) Jack Brabham", "d) Giuseppe Farina"],
     v: ["incorrect", "incorrect", "incorrect", "correct"],
-    
-
 }
-var qArr = [q1, q2, q3, q4];
+
+var q5 = {
+    q: "When and where was 'The Flying Finn' Mika Hakkinen's first victory?",
+    a: ["a) GP of Europe 1997", "b) GP of Brazil 1996", "c) GP of USA 1998", "d) GP of Italy 1997"],
+    v: ["correct", "incorrect", "incorrect", "incorrect"],
+}
+
+var q6 = {
+    q: "Which of these years did Damon Hill win the World Championship?",
+    a: ["a) 1997", "b) 1994", "c) 1996", "d) All listed"],
+    v: ["incorrect", "incorrect", "correct", "incorrect"],
+}
+
+var q7 = {
+    q: "Which of the following Brazilian F1 drivers have won the 24 Hours of Le Mans?",
+    a: ["a) Carlos Pace", "b) Raul Boesel", "c) Nelson Piquet", "d) None listed"],
+    v: ["incorrect", "incorrect", "incorrect", "correct"],
+}
+
+var q8 = {
+    q: "Alberto Ascari won how many F1 Championships?",
+    a: ["a) 2 Championships", "b) 1 Championship", "c) None", "d) 3 Championships"],
+    v: ["correct", "incorrect", "incorrect", "incorrect"],
+}
+
+var q9 = {
+    q: "Lewis Hamilton won his first F1 world championship title with which team?",
+    a: ["a) Ferrari", "b) Williams", "c) McLaren", "d) Red Bull"],
+    v: ["incorrect", "incorrect", "correct", "incorrect"],
+}
+
+var q10 = {
+    q: "Which was the former name of Sport PesaRacing Point F1 Yeam",
+    a: ["a) Sauber Motorsport", "b) Lotus F1", "c) Toro Rosso Racing", "d) Sahara Force India"],
+    v: ["incorrect", "incorrect", "incorrect", "correct"],
+}
+
+
+var qArr = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 var count = 0;
 var interval;
 var correct = 0;
@@ -42,22 +76,31 @@ var answer;
 var toInShowQ;
 
 $("#title").text("Trivia Game");
-$("#instructions").html("This trivia game is about sports knowledge. <br> <h2>You wil have 20 seconds select the correct answer. There are 10 questions, at the endo of the trivia your results will be shown</h2>")
-$("#new-btn").hide();
-
+$("#instructions").html("This trivia game is about Formula One &#8482 knowledge. <br> <h3>You wil have about 15 seconds to select your answer. There are 10 questions, at the end of the trivia your results will be shown.</h2>")
+$("#start-btn").css({ display: "initial" });
 
 $("#start-btn").click(start);
+$("#new-btn").click(start);
 
 
-function start(){  
-   showQ(count);
-   
-    interval = setInterval(pause,8000); // interval time sam as the sum of te tiemouts   
+function start() {
+    count = 0;
+    interval;
+    correct = 0;
+    incorrect = 0;
+    unanswered = 0;
+    choosing = true;
+    gameOver = false;
+    answer;
+    toInShowQ;
+    $("#gameOver").hide();
+    showQ(count);
 }
 
+
 function showQ(x) {
-   choosing=true;
-    answer="";
+    console.log("counter es " + count);
+    answer = "";
     $("#instructions-cont").empty();
     $("#good, #wrong").empty();
     $(".question-cont, .answers-cont").show();
@@ -68,114 +111,102 @@ function showQ(x) {
     $("#a2").text(qArr[x].a[1]);
     $("#a3").text(qArr[x].a[2]);
     $("#a4").text(qArr[x].a[3]);
-console.log("ans is" + answer);
-console.log("respueste en bd"+ qArr[x].v[0]);
-    toInShowQ= setTimeout(showR,5000);
-    }//end of showQ
-         $("#a1").click(function () {
-            answerChosen(count,0);
-        });
+    toInShowQ = setTimeout(showR, 3000);
+}
 
-        $("#a2").click(function () {
-            answerChosen(count,1);
-        });
 
-        $("#a3").click(function () {
-            answerChosen(count,2);
-        });
-        $("#a4").click(function () {
-            answerChosen(count,3);
-        });
+$("#a1").click(function () {
+    answerChosen(count, 0);
+});
 
-            function answerChosen(x,vIndex){
-            choosing = false;
-           clearTimeout(toInShowQ);
-             if (qArr[x].v[vIndex] == "correct") {
-                //correct++  
-              answer="correct";
-             }
-            if (qArr[x].v[vIndex] == "incorrect"){
-                //incorrect++
-              
-                                answer="incorrect" ;           
-        }
-        
-         console.log(answer);
-         console.log(correct);
-         console.log(incorrect);
-         showR();
+$("#a2").click(function () {
+    answerChosen(count, 1);
+});
 
-        }
-    
- 
-function showR(){ 
-        console.log("pause"+count);
-    if(answer=="correct"){
-       correct++
-        $("#good").html("<h1 >Great! Keep it going</h1>");
-    $(".question-cont, .answers-cont").hide();
+$("#a3").click(function () {
+    answerChosen(count, 2);
+});
+$("#a4").click(function () {
+    answerChosen(count, 3);
+});
+
+
+function answerChosen(x, vIndex) {
+    clearTimeout(toInShowQ);
+    if (qArr[x].v[vIndex] == "correct") {
+        answer = "correct";
     }
-    else if(answer=="incorrect"){
+    if (qArr[x].v[vIndex] == "incorrect") {
+        answer = "incorrect";
+    }
+    showR();
+}
+
+
+function showR() {
+    console.log("ShowR" + count);
+    setTimeout(pause, 1000)
+    //debugger;
+    if (answer == "correct") {
+        correct++
+        $("#good").html("<h1 >Great! Keep it going</h1>");
+        $(".question-cont, .answers-cont").hide();
+        console.log("pause" + count);
+    }
+    else if (answer == "incorrect") {
         incorrect++
         $("#wrong").html("<h1 >Wrong! The correct answer was:</h1>");
         ansCorrect(count);
-    $(".question-cont, .answers-cont").hide();
+        $(".question-cont, .answers-cont").hide();
+        console.log("pause" + count);
     }
-
-    else if(answer!="correct" && answer!="incorrect"){
-          $("#wrong").html("<h1 >Time's Up! The correct answer was:</h1>");
-          ansCorrect(count);
-          $(".question-cont, .answers-cont").hide();
-    console.log("pause");
-    }
-    if(gameOver){
-        setTimeout(showE,3000);
+    else if (answer != "correct" && answer != "incorrect") {
+        $("#wrong").html("<h1 >Time's Up! The correct answer was:</h1>");
+        ansCorrect(count);
+        $(".question-cont, .answers-cont").hide();
+        console.log("pause" + count);
     }
 }
 
 
 function pause() {
-    
-    
-    count++; 
-     showQ(count);
-     if (count == qArr.length-1) {
-        gameOver=true;
-        clearInterval(interval);
-               } 
-               
+    if (count == qArr.length - 1) {
+        gameOver = true;
+        showE();
+    }
+    if (!gameOver) {
+        count++;
+        showQ(count);
+    }
 }
 
 
-function showE(){
-       unanswered = qArr.length - correct - incorrect;
-        $(".question-cont").empty();
-        $(".answers-cont").empty();
-        $("#good, #wrong").empty();
-        $("#gameOver-text").html("End of The Game. <br> Here are Your Stats");
-        $("#correct").text(correct);
-        $("#incorrect").text(incorrect);
-        $("#unanswered").text(unanswered);
-        $("#new-btn").show();
-        
-        console.log(correct);
-        console.log(incorrect);
-        console.log(unanswered);
-        
-        console.log("gameover");
-    
+function showE() {
+    unanswered = qArr.length - correct - incorrect;
+    $(".question-cont").hide();
+    $(".answers-cont").hide();
+    $("#good, #wrong").empty();
+    $("#gameOver").show();
+    $("#gameOver-text").html("End of The Game. <br> Here are Your Stats");
+    $("#correct").text("Correct answers: " + correct);
+    $("#incorrect").text("Incorrect answers: " + incorrect);
+    $("#unanswered").text("Unanswered Questions" + unanswered);
+    $("#new-btn").css({ display: "initial" });
+
+    console.log(correct);
+    console.log(incorrect);
+    console.log(unanswered);
+    console.log("gameover");
 }
 
 
-function ansCorrect(x){
+function ansCorrect(x) {
+    for (var h = 0; h < qArr[x].v.length; h++) {
+        if (qArr[x].v[h] == "correct") {
+            var ansCorrect = $("<h2 id='ansCorrect'>");
+            ansCorrect.text(qArr[x].a[h]);
+            $("#wrong").append(ansCorrect);
 
-for (var h = 0; h < qArr[x].v.length; h++) {
-    
-    if(qArr[x].v[h]=="correct"){
-        var ansCorrect = $("<h2 id='ansCorrect'>");
-        ansCorrect.text(qArr[x].a[h]);
-        $("#wrong").append(ansCorrect);
-        
         }
     }
 }
